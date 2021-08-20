3d09b10a0dbddeb87a8c2f4223ed7a0896b1ee7d
Raj Patel <rajpatel@Rajs-MacBook-Pro.local>
2021-08-20 12:38:26 -0400

    NavBar for Lambda Pizza page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { NavBar } from "./components/navbar";
 
 const App = () => {
   return (
     <>
+    <NavBar />
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
     </>
